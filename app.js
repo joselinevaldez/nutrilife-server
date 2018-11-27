7fb142274e6674cd7f161c623dbe4ca7c4dc6529
joselinevg4@gmail.com <joselinevg4@gmail.com>
2018-11-26 21:42:20 -0700

    ULTIMA VERSION CON MONGO

--- app.js
+++ app.js
@@ -34,7 +34,9 @@ var alimentospRoutes = require('./routes/alimentosp');
 var dietasRoutes = require('./routes/dieta');
 var dietapacienteRoutes = require('./routes/dietapaciente');
 //conexion base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/nutrilife',(err,res)=>{
+//mongoose.connection.openUri('mongodb://localhost:27017/nutrilife',(err,res)=>{
+    mongoose.connection.openUri('mongodb://joseline:joseline04@ds151927.mlab.com:51927/nutrilife',(err,res)=>{
+    
     if (err) throw err;
     console.log('base de datos  \x1b[32m%s\x1b[0m',' online');
 });
