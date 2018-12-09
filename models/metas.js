888c24939da7f7ce93e97e889afed03a0870a384
joselinevg4@gmail.com <joselinevg4@gmail.com>
2018-12-09 01:36:15 -0700

    BACKEND-ULTIMA-VERSION-2

--- models/metas.js
+++ models/metas.js
@@ -4,22 +4,22 @@ var Schema = mongoose.Schema;
 
 var metasSchema = new Schema({
     abdomen: {type: Boolean, default:false},
-    abdomenmeta: {type:String, default:""},
+    abdomenmeta: {type:String, default:null},
     abdomencm: {type: Number, default:null},
     cintura: {type: Boolean, default:false},
-    cinturameta: {type:String, default:""},
+    cinturameta: {type:String, default:null},
     cinturacm: {type: Number, default:null},
     peso: {type: Boolean, default:false},
-    pesometa: {type:String, default:""},
+    pesometa: {type:String, default:null},
     pesokg: {type: Number, default:null},
     
     brazo: {type: Boolean, default:false},
-    brazometa: {type:String, default:""},
+    brazometa: {type:String, default:null},
     brazocm: {type: Number, default:null},
 
     
     gluteo: {type: Boolean, default:false},
-    gluteometa: {type:String, default:""},
+    gluteometa: {type:String, default:null},
     gluteocm: {type: Number, default:null},
     paciente:{ type: Schema.Types.ObjectId,ref:'Paciente',required:true} ,
     nutriologo:{ type: Schema.Types.ObjectId,ref:'Usuario',required:true}  
